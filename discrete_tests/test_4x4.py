27accced5c176f3b01bd7bfa172607e887215a49
grinvolod13 <grinvols@gmail.com>
2022-10-06 02:38:15 +0300

    Змінено тест

--- discrete_tests/test_4x4.py
+++ discrete_tests/test_4x4.py
@@ -5,20 +5,20 @@ import hopfield
 n = hopfield.Hopfield()
 
 learn_data = np.array(
-    [[1.0, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-     [-1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1]])
+    [[1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+     [-1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1]], np.float64)
 
-""",
-     [-1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1],
-     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1]
-"""
+plt.imshow([learn_data[0]])
+plt.show()
+plt.imshow([learn_data[1]])
+plt.show()
 
-inp = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1],np.float64)
+inp = np.array([1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, 1], np.float64)
 print(inp)
 plt.imshow([inp])
 plt.show()
 
-t, out = n.start(learn_data, inp)
+t, out = n.start(learn_data, inp, 10000)
 
 print("t:", t)
 print(out)
